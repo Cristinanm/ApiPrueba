4003007f196631d9a9fbd524fc8c592e079d9b57
Cristina <anajeram@miumg.edu.gt>
2026-06-12 19:53:45 -0600

    Primera API REST

---
+++ ApiPrueba/ApiPrueba/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.MapGet("/saludo", () =>
+{
+    return Results.Ok(new
+    {
+        mensaje = "Hola desde ApiPrueba",
+        fecha = DateTime.Now
+    });
+});
+
+app.Run();
